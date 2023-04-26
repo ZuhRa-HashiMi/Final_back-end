91f3b6d93e21d81b9dfa5657f74f4a4d07e9cfda
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-26 12:43:34 +0430

    Add validates and relation into doctor class

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,7 @@
 class Doctor < ApplicationRecord
+    has_many :appointments, dependent: :destroy, foreign_key: 'doctor_id'
+
+    validates :fullname, presence: true
+    validates :specialization, presence: true
+    validates :profile_picture, presence: true
 end
