b880ce49c4848d1f80afe73ea9a0c6b4ab108f40
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-26 12:23:39 +0430

    Add devise for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'user/index'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # root "home#index"
 end
