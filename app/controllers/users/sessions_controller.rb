8caa4f8ebbd8c30cc8264b585ae52ab9f821bfe9
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-27 17:29:59 +0430

    Add session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,33 @@
-# frozen_string_literal: true
+module Users
+  class SessionsController < Devise::SessionsController
+    respond_to :json
 
-class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+    private
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+    def current_token
+      request.env['warden-jwt_auth.token']
+    end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+    def respond_with(resource, _opts = {})
+      render json: {
+        status: { code: 200, message: 'Logged in sucessfully.' },
+        data: resource,
+        accessToken: current_token
+      }, status: :ok
+    end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    def respond_to_on_destroy
+      if current_user
+        render json: {
+          status: 200,
+          message: 'logged out successfully'
+        }, status: :ok
+      else
+        render json: {
+          status: 401,
+          message: "Couldn't find an active session."
+        }, status: :unauthorized
+      end
+    end
+  end
 end
