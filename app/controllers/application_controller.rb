1505a96ab86ad70d0592baafe3e651a33486fbba
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-27 17:29:03 +0430

    Add before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-    def current_user 
-        @current_user ||= User.find_by(id: session[:user_id])
-    end
-    helper_method :current_user
+    before_action :authenticate_user!
+    
 end
