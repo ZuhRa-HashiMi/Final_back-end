77e5967461dd5b0193c11b9c9b3dac2acfcade5d
shella12 <ayeshaarshad4567@gmail.com>
2023-05-01 12:09:05 +0500

    Fix errors

--- test/controllers/api/v1/appointments_controller_test.rb
+++ test/controllers/api/v1/appointments_controller_test.rb
@@ -1,37 +1,37 @@
-require "test_helper"
+require 'test_helper'
 
 class Api::V1::AppointmentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get api_v1_appointments_index_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get api_v1_appointments_show_url
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get api_v1_appointments_create_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get api_v1_appointments_new_url
     assert_response :success
   end
 
-  test "should get destroy" do
+  test 'should get destroy' do
     get api_v1_appointments_destroy_url
     assert_response :success
   end
 
-  test "should get update" do
+  test 'should get update' do
     get api_v1_appointments_update_url
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get api_v1_appointments_edit_url
     assert_response :success
   end
