8f101765d135acedb768e78ffcf449a5f3faa102
shella12 <ayeshaarshad4567@gmail.com>
2023-05-03 09:12:51 +0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
-    devise_for :users
+    devise_for :users, controllers: { sessions: :sessions },
+                       path_names: { sign_in: :login }
+
+    resource :user, only: [:show, :update]
   end
 
   mount Rswag::Ui::Engine => '/api-docs'
